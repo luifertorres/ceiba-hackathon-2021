9197768dd57ded1492405ed642cf0078ffb9ffeb
Cristian Garcia <criegt@outlook.com>
2021-11-13 10:29:34 -0500

    Separa a controlador

---
+++ src/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApi.Controllers;
+
+[Route("")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private static readonly JsonSerializerOptions s_jsonOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync([FromQuery] int number, [FromServices] IHttpClientFactory factory, [FromServices] IMemoryCache cache)
+    {
+        if (cache.TryGetValue(number, out var json))
+        {
+            return Content((string)json, MediaTypeNames.Application.Json);
+        }
+
+        var client = factory.CreateClient("Api");
+        var response = await client.GetAsync($"/?number={number}");
+
+        var content = await response.Content.ReadAsStringAsync();
+        var apiResponse = JsonSerializer.Deserialize<ApiResponse>(content, s_jsonOptions);
+
+        cache.Set(number, content, TimeSpan.FromSeconds(apiResponse!.ValiditySeconds));
+
+        return Content(content, MediaTypeNames.Application.Json);
+    }
+    record ApiResponse(DateTime Date, int ValiditySeconds);
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -4,37 +4,20 @@ builder.Services.AddHealthChecks();
 builder.Services.AddHttpClient("Api", c => 
     {
         c.Timeout = TimeSpan.FromMilliseconds(1000);
-        c.BaseAddress = new Uri($"http://{builder.Configuration["SVC_API_HOSTNAME"]}/{builder.Configuration["SVC_API_PORT"]}");
+        //c.BaseAddress = new Uri($"http://{builder.Configuration["SVC_API_HOSTNAME"]}/{builder.Configuration["SVC_API_PORT"]}");
+        c.BaseAddress = new("http://api-1.hack.local/");
     })
     .AddPolicyHandler(GetCircuitBreakerPolicy());
 builder.Services.AddMemoryCache();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.MapHealthChecks("/healthz");
-
-app.MapGet("/", async ([FromQuery] int number, [FromServices] IHttpClientFactory factory, [FromServices] IMemoryCache cache) =>
-{
-    if (cache.TryGetValue(number, out var json))
-    {
-        return new StringContent((string)json, Encoding.UTF8, MediaTypeNames.Application.Json);
-    }
-
-    var client = factory.CreateClient("Api");
-    var response = await client.GetAsync($"/?number={number}");
-
-    var content = await response.Content.ReadAsStringAsync();
-    var apiResponse = JsonSerializer.Deserialize<ApiResponse>(content);
-
-    cache.Set(number, content, TimeSpan.FromSeconds(apiResponse!.ValiditySeconds));
-
-    return new StringContent(content, Encoding.UTF8, MediaTypeNames.Application.Json);
-});
+app.MapControllers();
 
 app.Run();
 
 static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy() => HttpPolicyExtensions
     .HandleTransientHttpError()
     .CircuitBreakerAsync(1, TimeSpan.FromSeconds(30));
-
-record ApiResponse(DateTime Date, int ValiditySeconds);
